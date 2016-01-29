d3f18a1f72085b6bac909f028fb5434e4e6f5eb8
Maarten Vandeperre <maarten.vandeperre@gmail.com>
2016-01-29 13:42:53 +0100

    add text wrap util

---
+++ src/main/java/com/viae/common/pdf/TextWrapUtil.java
@@ -0,0 +1,112 @@
+package com.viae.common.pdf;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.pdfbox.pdmodel.font.PDFont;
+
+/**
+ * Utility class to handle text wrapping within the pdf.
+ *
+ * @author Vandeperre Maarten
+ */
+public class TextWrapUtil {
+
+    /**
+     * Wrap the given text (regarding the given context).
+     *
+     * @param text, the text you want to wrap
+     * @param fontFamily, the used font family (influences the width of the text string).
+     * @param fontSize, the used font size (influences the width of the text string).
+     * @param maxLineWidth, the maximum width a text string should have within the pdf.
+     * @return the wrap result: a combination of the max number of lines (i.e. how many lines were split of) and the lines them self.
+     */
+    public static WrapResult<List<String>> wrapText(final String text, final PDFont fontFamily, final float fontSize, final float maxLineWidth) {
+        final List<String> result = new LinkedList<>();
+        final float charWidth = getCharWidth(fontFamily, fontSize);
+        final int maxCharAmount = (int) (maxLineWidth / charWidth);
+
+        if(text != null){
+            String tempText = text;
+            while(tempText.length() > 0){
+                final int endIndex = maxCharAmount > tempText.length() ? tempText.length() : maxCharAmount;
+                final int lastSpaceIndex = tempText.substring(0, endIndex).trim().lastIndexOf(" ");
+                String toAdd = tempText;
+                if(lastSpaceIndex != -1 && tempText.length() > maxCharAmount){
+                    toAdd = tempText.substring(0, lastSpaceIndex + 1);
+                    tempText = tempText.substring(lastSpaceIndex + 1);
+                    result.add(toAdd);
+                } else {
+                    result.add(tempText);
+                    tempText = "";
+                }
+            }
+        }
+        return new WrapResult<>(result.size(), result);
+    }
+
+    /**
+     * Wrap the given text lines (regarding the given context).
+     *
+     * @param text, the lines of text you want to wrap
+     * @param fontFamily, the used font family (influences the width of the text string).
+     * @param fontSize, the used font size (influences the width of the text string).
+     * @param maxLineWidth, the maximum width a text string should have within the pdf.
+     * @return the wrap result: a combination of the max number of lines and the lines them self.
+     *  E.g. you provide two strings, the first one is 10 chars, the second one 5.
+     *  you want to wrap on a width corresponding to 6 characters:
+     *  than max number of lines will be 2 (from the first result).
+     *  than lines will contain 2 lines at index 0, and 1 line at index 1.
+     */
+    public static WrapResult<List<List<String>>> wrapText(final String[] textArray, final PDFont fontFamily, final float fontSize, final float maxLineWidth) {
+        final List<List<String>> result = new LinkedList<>();
+        int maxAmountOfLines = 0;
+        if(textArray != null){
+            for(final String text : textArray){
+                final WrapResult<List<String>> wrapResult = wrapText(text, fontFamily, fontSize, maxLineWidth);
+                maxAmountOfLines = Math.max(maxAmountOfLines, wrapResult.getMaxNumberOfLines());
+                result.add(wrapResult.getWrapResult());
+            }
+        }
+        return new WrapResult<>(maxAmountOfLines, result);
+    }
+
+    private static float getCharWidth(final PDFont fontFamily, final float fontSize) {
+        return fontFamily.getFontDescriptor().getFontBoundingBox().getWidth() / 1000 * fontSize;
+    }
+
+    /**
+     * Result of text wrapping.
+     *
+     * @author Vandeperre Maarten
+     *
+     * @param <RESULT_TYPE>, the type of the wrap result.
+     */
+    public static final class WrapResult<RESULT_TYPE> {
+        private final int maxNumberOfLines;
+        private final RESULT_TYPE wrapResult;
+
+        private WrapResult(final int maxNumberOfLines, final RESULT_TYPE wrapResult) {
+            this.maxNumberOfLines = maxNumberOfLines;
+            this.wrapResult = wrapResult;
+        }
+
+        /**
+         * @return the number of lines of the text entry which was split the most.
+         *
+         * E.g. you provide two strings, the first one is 10 chars, the second one 5.
+         *  you want to wrap on a width corresponding to 6 characters:
+         *  than max number of lines will be 2 (from the first result).
+         */
+        public int getMaxNumberOfLines() {
+            return maxNumberOfLines;
+        }
+
+        /**
+         * @return the result of the text wrap
+         */
+        public RESULT_TYPE getWrapResult() {
+            return wrapResult;
+        }
+    }
+}
---
+++ src/test/java/com/viae/common/pdf/TextWrapUtilTest.java
@@ -0,0 +1,123 @@
+package com.viae.common.pdf;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.junit.Test;
+
+import com.viae.common.pdf.TextWrapUtil.WrapResult;
+
+public class TextWrapUtilTest {
+    final String DUMMY_TEXT_SHORT = "Maarten Vandeperre";
+    final String DUMMY_TEXT_LONG = "Zolder vormt zondag het decor voor de strijd om de regenboogtrui in het veld. Volgt Mathieu van der Poel zichzelf op? Troeft Wout van Aert de Nederlandse titelverdediger af? Of is er een derde hond? Breng hier uw stem uit.";
+
+    @Test
+    public void coverageTest(){
+        assertNotNull(new TextWrapUtil());
+    }
+
+    @Test
+    public void testWrapTextForNullString(){
+        final String text = null;
+        final WrapResult<List<String>> result = TextWrapUtil.wrapText(text, PDType1Font.COURIER, 1, 1);
+        assertNotNull(result);
+        assertEquals(0, result.getMaxNumberOfLines());
+        assertEquals(Collections.emptyList(), result.getWrapResult());
+    }
+
+    @Test
+    public void testWrapTextForEmptyString(){
+        final String text = "";
+        final WrapResult<List<String>> result = TextWrapUtil.wrapText(text, PDType1Font.COURIER, 1, 1);
+        assertNotNull(result);
+        assertEquals(0, result.getMaxNumberOfLines());
+        assertEquals(Collections.emptyList(), result.getWrapResult());
+    }
+
+    @Test
+    public void testWrapTextForSpacelessString(){
+        final String text = "test";
+        final WrapResult<List<String>> result = TextWrapUtil.wrapText(text, PDType1Font.COURIER, 1, 1);
+        assertNotNull(result);
+        assertEquals(1, result.getMaxNumberOfLines());
+        assertEquals("test", result.getWrapResult().get(0));
+    }
+
+    @Test
+    public void testWrapTextForNullArray(){
+        final String[] textArray = null;
+        final WrapResult<List<List<String>>> result = TextWrapUtil.wrapText(textArray, PDType1Font.COURIER, 1, 1);
+        assertNotNull(result);
+        assertEquals(0, result.getMaxNumberOfLines());
+        assertEquals(Collections.emptyList(), result.getWrapResult());
+    }
+
+    @Test
+    public void testWrapTextForSingleString() {
+        //GIVEN
+        final PDFont fontFamily = PDType1Font.COURIER;
+        final float fontSize = 5;
+        final float maxLineWidth = 207.43782f;
+        //WHEN
+        final TextWrapUtil.WrapResult<List<String>> wrapResult = TextWrapUtil.wrapText(DUMMY_TEXT_LONG, fontFamily, fontSize, maxLineWidth);
+        final List<String> result = wrapResult.getWrapResult();
+        //THEN
+        assertEquals(5, wrapResult.getMaxNumberOfLines());
+        assertEquals(5, result.size());
+        final Iterator<String> it = result.iterator();
+        assertEquals("Zolder vormt zondag het decor voor de strijd om de ", it.next());
+        assertEquals("regenboogtrui in het veld. Volgt Mathieu van der Poel ", it.next());
+        assertEquals("zichzelf op? Troeft Wout van Aert de Nederlandse ", it.next());
+        assertEquals("titelverdediger af? Of is er een derde hond? Breng ", it.next());
+        assertEquals("hier uw stem uit.", it.next());
+    }
+    @Test
+    public void testWrapTextForStringArray() {
+        //GIVEN
+        final PDFont fontFamily = PDType1Font.COURIER;
+        final float fontSize = 5;
+        final float maxLineWidth = 207.43782f;
+        final String[] input = new String[]{DUMMY_TEXT_SHORT, DUMMY_TEXT_SHORT, DUMMY_TEXT_LONG, DUMMY_TEXT_SHORT, DUMMY_TEXT_SHORT};
+        //WHEN
+        final TextWrapUtil.WrapResult<List<List<String>>> wrapResult = TextWrapUtil.wrapText(input, fontFamily, fontSize, maxLineWidth);
+        final List<List<String>> result = wrapResult.getWrapResult();
+        //THEN
+        assertEquals(5, wrapResult.getMaxNumberOfLines());
+        assertEquals(5, result.size());
+        final Iterator<List<String>> listIt = result.iterator();
+
+        final Collection<String> firstResult = listIt.next();
+        assertEquals(1, firstResult.size());
+        assertEquals("Maarten Vandeperre", firstResult.iterator().next());
+
+        final Collection<String> secondResult = listIt.next();
+        assertEquals(1, secondResult.size());
+        assertEquals("Maarten Vandeperre", secondResult.iterator().next());
+
+        final Collection<String> thirdResult = listIt.next();
+        assertEquals(5, thirdResult.size());
+        final Iterator<String> thirdResultIt = thirdResult.iterator();
+        assertEquals("Zolder vormt zondag het decor voor de strijd om de ", thirdResultIt.next());
+        assertEquals("regenboogtrui in het veld. Volgt Mathieu van der Poel ", thirdResultIt.next());
+        assertEquals("zichzelf op? Troeft Wout van Aert de Nederlandse ", thirdResultIt.next());
+        assertEquals("titelverdediger af? Of is er een derde hond? Breng ", thirdResultIt.next());
+        assertEquals("hier uw stem uit.", thirdResultIt.next());;
+
+        final Collection<String> fourthResult = listIt.next();
+        assertEquals(1, fourthResult.size());
+        assertEquals("Maarten Vandeperre", fourthResult.iterator().next());
+
+        final Collection<String> fifthResult = listIt.next();
+        assertEquals(1, fifthResult.size());
+        assertEquals("Maarten Vandeperre", fifthResult.iterator().next());
+
+    }
+
+}
